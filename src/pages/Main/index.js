f7465dd59a5e261e8cf617cc43904cde80a2a3f9
Fernanda Esteves <fernanda.esteves@linx.com.br>
2020-01-06 12:09:25 -0300

    feat: animated title
    animation on keyboard event

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, TouchableWithoutFeedback, Keyboard, Platform, KeyboardAvoidingView, Animated } from 'react-native';
 import { Container, Title } from './styles';
 import TextInputBtn from '../../components/TextInputBtn/index';
 import SimpleButton from '../../components/SimpleButton/index';
@@ -7,6 +7,34 @@ import ConversionText from '../../components/ConversionText/index';
 import Header from '../../components/Header/index';
 
 const Main = ({ }) => {
+
+  const [sizeAnimated] = useState(new Animated.Value(50));
+  const ANIMATION_DURATION = 250;
+
+  useEffect(() => {
+    const name = Platform.OS === 'ios' ? 'Will' : 'Did';
+    Keyboard.addListener(`keyboard${name}Show`, keyboardShow);
+    Keyboard.addListener(`keyboard${name}Hide`, keyboardHide);
+  }, []);
+
+  function keyboardShow() {
+    // console.log('asdf');
+    Animated.timing(sizeAnimated, {
+      toValue: 20,
+      duration: ANIMATION_DURATION,
+    }).start();
+
+  }
+
+  function keyboardHide() {
+    // console.log('hide');
+    Animated.timing(sizeAnimated, {
+      toValue: 50,
+      duration: ANIMATION_DURATION,
+    }).start();
+
+  }
+
   const TEMP_BASE_CURRENCY = 'USD';
   const TEMP_QUOTE_CURRENCY = 'GBP';
   const TEMP_BASE_PRICE = '100';
@@ -19,6 +47,7 @@ const Main = ({ }) => {
       <Container>
         <Header onPress={() => { }} />
 
+        <Animated.Text style={{ fontSize: sizeAnimated, color: '#fff' }}>Teste</Animated.Text>
         <Title>Currency Converter</Title>
         <KeyboardAvoidingView>
           <TextInputBtn
